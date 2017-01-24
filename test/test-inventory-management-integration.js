a719662ee0d8a53ad0c9c0f73f1fd3f80805135c
steve2482 <stevecb12684@yahoo.com>
2017-01-23 22:06:01 -0500

    test added to verify root url, still need to verify html response

---
+++ test/test-inventory-management-integration.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const should = chai.should();
+
+const {app, express} = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Root Url', function() {
+  it('should show the main html and send a 200 status code', function() {
+    return chai.request(app)
+    .get('/')
+    .then(function(res) {
+      res.should.have.status(200);
+      // res.should.include('html');
+    });
+  });
+});
