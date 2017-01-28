3f8486ced883c50e21154f43d25c9528d397006e
steve2482 <stevecb12684@yahoo.com>
2017-01-27 22:01:09 -0500

    setting up use with database

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-var express = require('express');
-var app = express();
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+
+app.use(bodyParser.json());
 
 app.use('/', express.static(__dirname + '/public'));
 
@@ -7,6 +15,41 @@ app.get('/inventory', (req, res) => {
   res.sendFile(__dirname + '/views/user-inventory.html');
 });
 
-app.listen(process.env.PORT || 8080);
+let server;
+function runServer(databaseURl = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
 
-module.exports = {app};
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
--- test/test-inventory-management-integration.js
+++ test/test-inventory-management-integration.js
@@ -1,28 +1,40 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
 
 const should = chai.should();
 
-const {app, express} = require('../server');
-
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} =require('../config');
 chai.use(chaiHttp);
 
-describe('Root Url', function() {
-  it('should show the main html and send a 200 status code', function() {
-    return chai.request(app)
-    .get('/')
-    .then(function(res) {
-      res.should.have.status(200);
-      res.should.be.html;
-    });
+describe('checking URLs', function() {
+
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
   });
 
-  it('should show the user-inventory html and send a 200 status code', function() {
-    return chai.request(app)
-    .get('/inventory')
-    .then(function(res) {
-      res.should.have.status(200);
-      res.should.be.html;
+  after(function() {
+    return closeServer();
+  });
+
+  describe('Root Url', function() {
+    it('should show the main html and send a 200 status code', function() {
+      return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.html;
+      });
+    });
+
+    it('should show the user-inventory html and send a 200 status code', function() {
+      return chai.request(app)
+      .get('/inventory')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.html;
+      });
     });
   });
 });
