495b18ed41eeb15e952ff0cd2c8afe7683dbd359
steve2482 <stevecb12684@yahoo.com>
2017-01-29 14:13:27 -0500

    dependencies added

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-					             global.DATABASE_URL ||
-					             'mongodb://localhost/storeful';
+                       global.DATABASE_URL ||
+                       'mongodb://localhost/storeful';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
   'mongodb://localhost/storeful-test-db');
