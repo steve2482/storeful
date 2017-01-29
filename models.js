19430d903cca190a8b117825f1c5d6b3a9fe3e71
steve2482 <stevecb12684@yahoo.com>
2017-01-29 14:13:43 -0500

    updated UserSchema

--- models.js
+++ models.js
@@ -1,11 +1,55 @@
+
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-	userName: {type: String, required: true},
-	password: {type: String, required: true},
-	items: []
+mongoose.Promise = global.Promise;
+
+const UserSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  inventory: [
+    {
+      id: {
+        type: Number
+      },
+      itemName: {
+        type: String,
+        required: true
+      },
+      unitPrice: {
+        type: Number
+      },
+      retailPrice: {
+        type: Number
+      },
+      balanceOnHand: {
+        type: Number,
+        required: true
+      },
+      requestedInventoryLevel: {
+        type: Number,
+        required: true
+      },
+      sold: {
+        type: Number
+      },
+      vendor: {
+        type: String,
+        required: true
+      }
+    }]
 });
 
-const User = mongoose.model('User', userSchema);
+UserSchema.methods.validatePassord = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
